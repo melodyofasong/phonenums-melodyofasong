bd603ef97182a9ef96c8b14e7fe13b630afe5720
melodyofasong <harshitamelody@gmail.com>
2026-03-23 22:50:40 +0530

    Refactor About.py to use relative path for markdown

--- pages/4- About.py
+++ pages/4- About.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-f = "E:\\Harshita's Explorations\\github\\phonenums-melodyofasong\\assets\\About.md"
-md_path = "E:\\Harshita's Explorations\\Classifieds phone numbers\\phonenums\\streamlit-phonenums-v1\\assets\\About.md"
-with open(f, "r") as f:
+with open(assets/About.md, "r") as f:
     content = f.read()
 st.markdown(content)
