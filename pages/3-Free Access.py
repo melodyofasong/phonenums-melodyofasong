2b2cf8d806f2acd616461ebd31e5bdf3c859f524
melodyofasong <harshitamelody@gmail.com>
2026-03-23 22:44:18 +0530

    Add files via upload

---
+++ pages/3-Free Access.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.subheader("Access to 1000 Free Phone Numbers")
+st.write("City-wise differentiation")
+# use urllib.request for webscraping method .urlopen.read()
+st.space()
+st.write("This project is still in progress and not open to the public eye yet.")
+st.space()
+st.caption("phonenums-melodyofasong")
