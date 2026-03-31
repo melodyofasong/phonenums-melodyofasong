93773a955e66e672c254dd3f2e9e918f176e780d
melody <harshitamelody@gmail.com>
2026-03-31 12:14:39 +0530

    Integrate payment options in Free Access page
    Added payment integration sidebar and footer.

--- pages/3-Free Access.py
+++ pages/3-Free Access.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from utils.paywithchai import paywithchai_sidebar, paywithchai_footer
 
 st.subheader("Access to 1000 Free Phone Numbers")
+
+with st.sidebar:
+	paywithchai_sidebar()
+
 st.write("City-wise differentiation")
 # use urllib.request for webscraping method .urlopen.read()
 st.space()
 st.write("This project is still in progress and not open to the public eye yet.")
-st.space()
-st.caption("phonenums-melodyofasong")
+
+paywithchai_footer()
